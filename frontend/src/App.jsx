5cc63c5f9f855d80352a401f7477f55c7dfe8e20
Fabian Gretsch <fabian.gretsch+github@gmail.com>
2026-07-07 21:34:27 +0200

    feat: add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import PokemonList from "./components/PokemonList";
 import PokemonDetail from "./components/PokemonDetail";
 import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<PokemonList />} />
       <Route path="/pokemon/:id" element={<PokemonDetail />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
