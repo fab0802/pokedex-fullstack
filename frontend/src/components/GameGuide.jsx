f419734c92ce60b53ad43e70fe751ec369e4de26
fab0802 <fabian.gretsch@preisigag.ch>
2026-08-10 12:45:09 +0200

    feat: add Pokémon Black/White (Unova) to game guide

--- frontend/src/components/GameGuide.jsx
+++ frontend/src/components/GameGuide.jsx
@@ -6,12 +6,13 @@ import yellow from "../data/trainers/yellow.json";
 import goldSilver from "../data/trainers/goldSilver.json";
 import rubySapphire from "../data/trainers/rubySapphire.json";
 import diamondPearl from "../data/trainers/diamondPearl.json";
+import blackWhite from "../data/trainers/blackWhite.json";
 import TrainerCard from "./TrainerCard";
 import styles from "./GameGuide.module.css";
 
 // Aktuell nur ein Guide - bewusst als Liste angelegt, damit weitere Spiele
 // später nur ergänzt werden müssen: Datendatei importieren und hier eintragen.
-const GUIDES = [redBlue, yellow, goldSilver, rubySapphire, diamondPearl];
+const GUIDES = [redBlue, yellow, goldSilver, rubySapphire, diamondPearl, blackWhite];
 
 const CATEGORY_ORDER = [
   "gym",
